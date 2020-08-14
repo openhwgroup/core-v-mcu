c5cd3895242f26dde3a8ac632ddbf6e3b487a33b
hpollittsmith <pollitt-smith@cmc.ca>
2020-08-14 11:18:48 -0400

    modified:   fc_subsystem.sv

--- fpga/cv32e40p_modified_files/fc_subsystem.sv
+++ fpga/cv32e40p_modified_files/fc_subsystem.sv
@@ -8,6 +8,7 @@
 // CONDITIONS OF ANY KIND, either express or implied. See the License for the
 // specific language governing permissions and limitations under the License.
 
+// Modified by Hugh Pollitt-Smith, CMC Microsystems
 
 `include "soc_bus_defines.sv"
 
