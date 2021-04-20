d2932797f70949ee778ca7b8e5d6cc0b40c665eb
Greg Martin <gmartin@quicklogic.com>
2021-04-20 10:43:38 -0500

    fix bidir in perdef.json and address width fixes
    Signed-off-by: Greg Martin <gmartin@quicklogic.com>

--- rtl/core-v-mcu/efpga_subsystem/efpga_subsystem.sv
+++ rtl/core-v-mcu/efpga_subsystem/efpga_subsystem.sv
@@ -279,7 +279,7 @@ module efpga_subsystem
        .push_clk(asic_clk_i),
        .push_rst_n(rst_n),
      .data_req_i(s_lint_REQ),
-     .data_add_i       ( apbt1_i.add[APB_FPGA_ADDR_WIDTH-1:0] ),
+     .data_add_i       ( apbt1_i.add),
       .data_wen_i       ( apbt1_i.wen                 ),
       .data_wdata_i     ( apbt1_i.wdata               ),
       .data_be_i        ( apbt1_i.be                  ),
@@ -291,7 +291,7 @@ module efpga_subsystem
       .data_r_ID_o      (                              ),
 
      .data_req_o       (apbt1_int.req                ),
-     .data_add_o       ( apbt1_int.add[ADDR_WIDTH-1:0]  ),
+     .data_add_o       ( apbt1_int.add ),
      .data_wen_o       ( apbt1_int.wen                  ),
      .data_wdata_o     ( apbt1_int.wdata                ),
      .data_be_o        ( apbt1_int.be                   ),
