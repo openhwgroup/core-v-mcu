11aa39b57a31ca892527f23b381ffd0a5feee081
Greg Martin <gmartin@quicklogic.com>
2021-06-14 08:01:59 -0500

    verible format for merge
    Signed-off-by: Greg Martin <gmartin@quicklogic.com>

--- rtl/includes/pulp_peripheral_defines.svh
+++ rtl/includes/pulp_peripheral_defines.svh
@@ -18,8 +18,8 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-`define BUILD_DATE 32'h20210608
-`define BUILD_TIME 32'h00162122
+`define BUILD_DATE 32'h20210611
+`define BUILD_TIME 32'h00093117
 
 //  PER_ID definitions
 `define PER_ID_UART      0
