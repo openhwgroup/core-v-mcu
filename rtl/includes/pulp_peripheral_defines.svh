41d481a9373e7d87db444b2de79b88264503bf8b
Greg Martin <gmartin@quicklogic.com>
2021-07-01 09:17:56 -0500

    removed offending GPL code
    Signed-off-by: Greg Martin <gmartin@quicklogic.com>

--- rtl/includes/pulp_peripheral_defines.svh
+++ rtl/includes/pulp_peripheral_defines.svh
@@ -18,8 +18,8 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-`define BUILD_DATE 32'h20210624
-`define BUILD_TIME 32'h00080027
+`define BUILD_DATE 32'h20210701
+`define BUILD_TIME 32'h00074605
 
 //  PER_ID definitions
 `define PER_ID_UART      0
