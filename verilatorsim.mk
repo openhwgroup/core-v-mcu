d90192cdf25130a86e25ee77b388d51aa995e7de
davide schiavone <davide@openhwgroup.org>
2022-07-18 16:53:55 +0200

    improvements from PR #230

--- verilatorsim.mk
+++ verilatorsim.mk
@@ -43,9 +43,9 @@ MODEL_LIB ?= ./obj_dir/Vcore_v_mcu_testharness__ALL.a
 
 CPP := gcc
 CXX := g++
-CPPFLAGS := -I$(VINCDIR) -I$(VINCDIR_DPI) -I./obj_dir -I../../../tb/uartdpi/ -DVL_TIME_CONTEXT -DVM_COVERAGE=0 -DVM_SC=0 -DVM_TRACE=1 -DVM_TRACE_FST=1 -faligned-new -fcf-protection=none -Wno-bool-operation -Wno-sign-compare -Wno-uninitialized -Wno-unused-but-set-variable -Wno-unused-parameter -Wno-unused-variable -Wno-shadow
-CXXFLAGS := -std=c++11 -Wall -g -fpermissive  -std=gnu++14 -O0 -g3
-LDFLAGS := -lz -pthread -lutil -lelf
+CPPFLAGS := -I$(VINCDIR) -I$(VINCDIR_DPI) -I./obj_dir -I../../../tb/uartdpi/ -DVL_TIME_CONTEXT -DVM_COVERAGE=0 -DVM_SC=0 -DVM_TRACE=1 -DVM_TRACE_FST=1 -Wno-bool-operation -Wno-sign-compare -Wno-uninitialized -Wno-unused-but-set-variable -Wno-unused-parameter -Wno-unused-variable -Wno-shadow -Wall
+CXXFLAGS := -std=c++11 -g -fpermissive  -std=gnu++14 -O0 -g3
+LDFLAGS := -lz -pthread -lutil
 
 CF_STYLE := "{BasedOnStyle: GNU, \
               AllowShortFunctionsOnASingleLine: InlineOnly, \
@@ -53,31 +53,35 @@ CF_STYLE := "{BasedOnStyle: GNU, \
 
 OBJS = core_v_mcu_tb.o
 
+USER_CPPFLAGS =
+USER_LDFLAGS =
+USER_CXXFLAGS =
+
 .PHONY: all
 all: mem_init core_v_mcu_tb.exe
 
 core_v_mcu_tb.exe: $(OBJS) verilated.o verilated_dpi.o verilated_fst_c.o uartdpi.o $(MODEL_LIB)
 	echo $(VBINDIR)
-	$(CXX) $^ $(LDFLAGS) -o $@
+	$(CXX) $^ $(LDFLAGS) $(USER_LDFLAGS) -o $@
 
 verilated.o: $(VERILATED_SRC) $(VERILATED_DEPS) $(MODEL_LIB)
-	$(CXX) $(CPPFLAGS) $(CXXFLAGS) -c -o $@ $(VERILATED_SRC)
+	$(CXX) $(CPPFLAGS) $(USER_CPPFLAGS) $(CXXFLAGS) $(USER_CXXFLAGS) -c -o $@ $(VERILATED_SRC)
 
 verilated_dpi.o: $(VERILATED_DPI_SRC) $(VERILATED_DEPS) $(MODEL_LIB)
-	$(CXX) $(CPPFLAGS) $(CXXFLAGS) -c -o $@ $(VERILATED_DPI_SRC)
+	$(CXX) $(CPPFLAGS) $(USER_CPPFLAGS) $(CXXFLAGS) $(USER_CXXFLAGS) -c -o $@ $(VERILATED_DPI_SRC)
 
 verilated_fst_c.o: $(VERILATED_FST_C_SRC) $(VERILATED_FST_C_DEPS) $(MODEL_LIB)
-	$(CXX) $(CPPFLAGS) $(CXXFLAGS) -c -o $@ $(VERILATED_FST_C_SRC)
+	$(CXX) $(CPPFLAGS) $(USER_CPPFLAGS) $(CXXFLAGS) $(USER_CXXFLAGS) -c -o $@ $(VERILATED_FST_C_SRC)
 
 mem_init:
 	$(RM) $@
 	ln -s ./mem_init $@
 uartdpi.o: ../../../tb/uartdpi/uartdpi.c
-	$(CXX) $(CPPFLAGS) $(CXXFLAGS)  -c -o $@ $<
+	$(CXX) $(CPPFLAGS) $(USER_CPPFLAGS) $(CXXFLAGS) $(USER_CXXFLAGS)  -c -o $@ $<
 
 # Object dependencies
 core_v_mcu_tb.o: ../../../tb/core_v_mcu_tb.cpp
-	$(CXX) $(CPPFLAGS) $(CXXFLAGS) -c -o $@ $<
+	$(CXX) $(CPPFLAGS) $(USER_CPPFLAGS) $(CXXFLAGS) $(USER_CXXFLAGS) -c -o $@ $<
 
 .PHONY: clean
 clean:
